9f4ae9f7b778320c9536994a0e2b459febc77b2d
dgo45921 <3023135080101@ingenieria.usac.edu.gt>
2024-02-06 20:27:45 -0600

    first commit !

---
+++ Practica1/practica1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+// compile command: gcc practica1.c -o practica1
+
+int main()
+{
+    // printf() displays the string inside quotation
+    printf("Hello, World!");
+    return 0;
+}
